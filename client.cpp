301d29c8cea92c8a17eb6fd18dd7a41f2c8be2c4
JollyCo-operation2527 <nghuyduc2527@gmail.com>
2025-07-04 05:21:58 -0400

    add logging (client side)

--- client.cpp
+++ client.cpp
@@ -10,6 +10,8 @@
 #include <vector>
 #include <thread>
 #include <sstream>
+#include <ctime>
+#include <mutex>
 
 #define SERVER_IP "127.0.0.1"
 #define SERVER_PORT 6000
@@ -21,6 +23,7 @@ int uploadFile(int, std::string);
 void handleUpload(int);
 void handleDownload(int);
 int downloadFile(int, std::string);
+void log(const std::string&);
 
 // Structure declaration
 struct FileHeader{
@@ -36,6 +39,10 @@ void createDownloadFolder(){
     }
 }
 
+// Declare logFile
+std::ofstream logFile("logClient.txt", std::ios::app);
+std::mutex logMutex;
+
 int main(){
     int clientSocket;
     struct sockaddr_in  serverAddress;
@@ -46,11 +53,14 @@ int main(){
     // Create a download folder if there is none
     createDownloadFolder();
 
+    // This is the start of new log
+    log("===============================================================================");
+
     // Create a client socket
     clientSocket = socket(AF_INET, SOCK_STREAM, 0);
 
     if (clientSocket < 0){
-        std::cout << "CLIENT ERROR: Could not open socket" << std::endl;
+        log("CLIENT ERROR: Could not open socket");
         exit(-1);
     }
 
@@ -64,7 +74,7 @@ int main(){
     status = connect(clientSocket, (struct sockaddr *) &serverAddress, sizeof(serverAddress));
 
     if (status < 0){
-        std::cout << "CLIENT ERROR: Could not connect to server" << std::endl;
+        log("CLIENT ERROR: Could not connect to server");
         exit(-1);
     }
 
@@ -81,10 +91,12 @@ int main(){
         // Send message string to server
         strcpy(buffer, inStr.c_str());
         std::cout << "CLIENT: Sending \"" << buffer << "\" to the server" << std::endl;
+        log("CLIENT: Sending \"" + std::string(buffer) + "\" to the server");
+
         int bytesSend = send(clientSocket, buffer, strlen(buffer), 0);
 
         if(bytesSend < 0){
-            std::cout << "CLIENT: bytesSend < 0 error" << std::endl;
+            log("CLIENT ERROR: bytesSend < 0 error");
         }
 
         if (strcmp(buffer, "stop") == 0){
@@ -107,15 +119,20 @@ int main(){
         // If receive less than or equal 0 bytes from server, it means the server connection is closed
         if (bytesRcv <= 0){
             std::cout << "CLIENT: Server connection closed" << std::endl;
+            log("CLIENT: Server connection closed");
             break;
         }
 
         buffer[bytesRcv] = 0;  // Put a 0 at the end so we can display the string
         std::cout << "CLIENT: Got back response \"" << buffer << "\" from the server" << std::endl;
+        log("CLIENT: Got back response \"" + std::string(buffer) + "\" from the server");
     }
 
     close(clientSocket);
     std::cout << "CLIENT: Shutting down" << std::endl;
+    log("CLIENT: Shutting down");
+
+    return 0;
 }
 
 void handleUpload(int clientSocket){
@@ -125,6 +142,7 @@ void handleUpload(int clientSocket){
     // Get the user to specify the file's name
     std::cout << "CLIENT: Enter file's name: ";
     std::getline(std::cin, input);  
+    log("CLIENT: Enter file's name: " + input);
 
     // Create a string stream object to input
     std::stringstream ss(input);
@@ -145,6 +163,7 @@ void handleUpload(int clientSocket){
             uploadFile(clientSocket, str);
         }else{
             std::cout << "CLIENT ERROR: No such file or file irregular" << std::endl;
+            log("CLIENT ERROR: No such file or file irregular");
             continue;
         }
     }
@@ -158,6 +177,7 @@ int uploadFile(int clientSocket, std::string input){
     // If the input is "abort"
     if(input == "abort"){
         std::cout << "CLIENT: Abort uploading mode" << std::endl;
+        log("CLIENT: Abort uploading mode");
         // The file's name is "abort". No real file is named like this 
         strcpy(myFile.fn, "abort");
         myFile.fs = 0;
@@ -170,6 +190,8 @@ int uploadFile(int clientSocket, std::string input){
         myFile.fs = 0;
         // Send this fake file to the server. The server will recognize "done" as the end of the list of files to upload
         std::cout << "CLIENT: Sending \"Done\"" << std::endl;
+        log("CLIENT: Sending \"Done\"");
+
         int bytesSend = send(clientSocket, &myFile, sizeof(myFile), 0);
         return 0;
     }
@@ -179,7 +201,7 @@ int uploadFile(int clientSocket, std::string input){
     
     // If cannot open inFile
     if (!inFile){
-        std::cout << "CLIENT: Could not open inFile" << std::endl;
+        log("CLIENT ERROR: Could not open inFile");
         return -1;
     }
 
@@ -195,9 +217,10 @@ int uploadFile(int clientSocket, std::string input){
     int bytesSend = send(clientSocket, &myFile, sizeof(myFile), 0);
 
     std::cout << "File: " << myFile.fn << " - " << myFile.fs << std::endl;
+    log("File: " + std::string(myFile.fn) + " - " + std::to_string(myFile.fs));
 
     if(bytesSend < 0){
-        std::cout << "CLIENT: bytesSend < 0 error (1)" << std::endl;
+        log("CLIENT ERROR: bytesSend < 0 error (1)");
     }
 
     // Read inFile into buffer
@@ -210,11 +233,12 @@ int uploadFile(int clientSocket, std::string input){
         bytesSend = send(clientSocket, buffer, bytesRead, 0);
 
         if(bytesSend < 0){
-            std::cout << "CLIENT: bytesSend < 0 error (2)" << std::endl;
+            log("CLIENT: bytesSend < 0 error (2)");
         }
     }
 
     std::cout << "File: " << input << " - Done" << std::endl;
+    log("File: " + input + " - Done");
 
     // Wait for the acknowledgement from the server
     char ackBuffer[80];
@@ -222,6 +246,7 @@ int uploadFile(int clientSocket, std::string input){
     if (ackBytes > 0){
         ackBuffer[ackBytes] = '\0';
         std::cout << "CLIENT: Server has received file: " << myFile.fn << std::endl;
+        log("CLIENT: Server has received file: " + std::string(myFile.fn));
     }
 
     return 0;
@@ -234,6 +259,7 @@ void handleDownload(int clientSocket){
     // Get the user to specify the file's name
     std::cout << "CLIENT: Enter file's name: ";
     std::getline(std::cin, input);  
+    log("CLIENT: Enter file's name: " + input);
 
     // Create a string stream object to input
     std::stringstream ss(input);
@@ -252,6 +278,7 @@ void handleDownload(int clientSocket){
     for(const std::string& str : filesToDownload){
         if(str == "abort"){
             std::cout << "CLIENT: Aborting Download Mode" << std::endl;
+            log("CLIENT: Aborting Download Mode");
             // Send "abort" string to the server
             send(clientSocket, str.c_str(), str.size(), 0);
             break;
@@ -264,6 +291,7 @@ void handleDownload(int clientSocket){
                 if (okBytes > 0){
                     okBuffer[okBytes] = '\0';
                     std::cout << "CLIENT: Receive response from server: " << okBuffer << std::endl;
+                    log("CLIENT: Receive response from server: " + std::string(okBuffer));
                 }
                 flag = 0;
             }
@@ -282,7 +310,7 @@ int downloadFile(int clientSocket, std::string input){
     int requestFile = send(clientSocket, input.c_str(), input.size() + 1, 0);  
 
     if (requestFile < 0){
-        std::cout << "CLIENT: requestFile < 0 error" << std::endl;
+        log("CLIENT ERROR: requestFile < 0 error");
     }
 
     // if input is "done", this is the end of filesToDownload
@@ -296,6 +324,7 @@ int downloadFile(int clientSocket, std::string input){
     // If fake file, return
     if (strcmp(recvFile.fn, "done") == 0){
         std::cout << "CLIENT: File requested does not exist or inaccessible" << std::endl;
+        log("CLIENT: File requested does not exist or inaccessible");
         return 0;
     }
 
@@ -303,16 +332,17 @@ int downloadFile(int clientSocket, std::string input){
     fullPath += recvFile.fn;
 
     std::cout << "CLIENT: Client downloading" << std::endl;
+    log("CLIENT: Client downloading");
 
     // Debugging purpose
-    std::cout << "CLIENT: File's name is: " << recvFile.fn << std::endl;
-    std::cout << "CLIENT: File's size is: " << recvFile.fs << std::endl;
+    log("CLIENT: File's name is: " + std::string(recvFile.fn));
+    log("CLIENT: File's size is: " + std::to_string(recvFile.fs));
     // Debugging purpose
 
     std::ofstream downFile(fullPath, std::ofstream::binary);
 
     if (!downFile){
-        std::cout << "CLIENT: Could not write downFile" << std::endl;
+        log("CLIENT ERROR: Could not write downFile");
         return -1;
     }
 
@@ -322,7 +352,7 @@ int downloadFile(int clientSocket, std::string input){
         int bytesRead = recv(clientSocket, buffer, bytesToRead, 0);
 
         if (bytesRead < 0){
-            std::cout << "CLIENT: Writing error" << std::endl;
+            log("CLIENT ERROR: Writing error");
             break;
         }
 
@@ -337,9 +367,19 @@ int downloadFile(int clientSocket, std::string input){
     int ackBytes = send(clientSocket, ackMsg, strlen(ackMsg), 0);
     if (ackBytes > 0){
         std::cout << "CLIENT: File " << recvFile.fn << " received" << std::endl;
+        log("CLIENT: File " + std::string(recvFile.fn) + " received");
     }
 
     downFile.close();
     std::cout << "CLIENT: File closed" << std::endl;
+    log("CLIENT: File closed");
     return 0;
+}
+
+// This function handles logging
+void log(const std::string& message){
+    std::lock_guard<std::mutex> guard(logMutex);
+
+    std::time_t now = std::time(nullptr);
+    logFile << std::ctime(&now) << ">> " << message << std::endl;
 }
